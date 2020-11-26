15d20dd72618c34f63103c83c847392e187c368d
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-11-26 16:15:16 -0600

    User (profile editor) GUI implemented

---
+++ app/src/main/java/com/mm543/awis/ui/user/UserActivity.kt
@@ -0,0 +1,54 @@
+/*
+ * AWIS: Adventure Works Information Systems, Android App
+ * Created for the MM543-0900-3-2020 course.
+ * Team: Tobias Briones, Juan Varela
+ *
+ * Copyright (c) 2020 Tobias Briones.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.mm543.awis.ui.user
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.mm543.awis.R
+import kotlinx.android.synthetic.main.content_user.*
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        user_name_text.setText("User Name")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_user, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> navigateBack()
+            R.id.action_save -> save()
+        }
+        return true
+    }
+
+    private fun navigateBack() {
+        onBackPressed()
+    }
+
+    private fun save() {
+
+    }
+}
