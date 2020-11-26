430bffbae9aaf203260d273d61de845142a02987
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-11-26 12:19:16 -0600

    MIT License added

--- app/src/main/java/com/mm543/awis/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/mm543/awis/domain/repository/ProductRepository.kt
@@ -1,3 +1,14 @@
+/*
+ * AWIS: Adventure Works Information Systems, Android App
+ * Created for the MM543-0900-3-2020 course.
+ * Team: Tobias Briones, Juan Varela
+ *
+ * Copyright (c) 2020 Tobias Briones.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package com.mm543.awis.domain.repository
 
 import com.mm543.awis.domain.model.Product
