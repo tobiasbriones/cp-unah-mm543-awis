902039bb07e6770d13f10a78cb5d2cbe96dc8584
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-12-05 20:53:11 -0600

    Navigation: Use android menu resources, enhancements

--- app/src/main/java/com/mm543/awis/MainActivity.kt
+++ app/src/main/java/com/mm543/awis/MainActivity.kt
@@ -13,32 +13,38 @@ package com.mm543.awis
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
 import com.mm543.awis.ui.checkout.CheckoutActivity
 import com.mm543.awis.ui.main.AboutDialog
 import com.mm543.awis.ui.main.SignInDialog
-import com.mm543.awis.ui.main.nav.NavDrawerItem
+import com.mm543.awis.ui.main.search.ProductSearchFragment
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var navigation: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navigation = findViewById(R.id.drawer)
+        navigation = findViewById(R.id.main_drawer)
+        navigationView = findViewById(R.id.activity_main_nav_view)
 
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+        navigationView.setNavigationItemSelectedListener(this)
+        showProductsFragment()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
@@ -72,8 +78,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        closeDrawer()
+        return when (item.itemId) {
+            R.id.main_drawer_categories -> {
+                // TODO
+                true
+            }
+            else -> true
+        }
+    }
+
     private fun showNavigation() {
         navigation.open()
+    }
+
+    private fun closeDrawer() {
+        navigation.closeDrawer(Gravity.LEFT)
     }
 
     private fun showSignInDialog() {
@@ -92,7 +113,14 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun closeDrawer(item: NavDrawerItem) {
-        navigation.closeDrawer(Gravity.LEFT)
+    private fun showProductsFragment() {
+        val fragment = ProductSearchFragment()
+        val fm: FragmentManager = supportFragmentManager
+        val ft = fm.beginTransaction()
+
+        ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+            .replace(R.id.main_content_frame, fragment)
+            .commit()
+        Log.d("App", "products")
     }
 }
