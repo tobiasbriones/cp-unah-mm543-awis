304bc0ddfa5b1fb0f50c674c54531dcd937b107e
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-12-06 19:37:41 -0600

    AboutDialog information and layout updated

--- app/src/main/java/com/mm543/awis/ui/main/AboutDialog.kt
+++ app/src/main/java/com/mm543/awis/ui/main/AboutDialog.kt
@@ -14,17 +14,23 @@ package com.mm543.awis.ui.main
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.mm543.awis.R
 
 class AboutDialog : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val inflater = requireActivity().layoutInflater
+        val view = inflater.inflate(R.layout.dialog_about, null)
+        val ghLinkTV = view.findViewById<TextView>(R.id.about_gh_link_tv)
+        ghLinkTV.movementMethod = LinkMovementMethod.getInstance()
+
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            val inflater = requireActivity().layoutInflater
 
-            builder.setView(inflater.inflate(R.layout.dialog_about, null))
+            builder.setView(view)
                 .setPositiveButton(
                     R.string.ok
                 ) { dialog, id ->
